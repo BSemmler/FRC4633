63338fd153a0087b194fbab577d3896e26548d93
trag1c <trag1c@users.noreply.github.com>
2014-01-15 23:31:11 -0700

    Create Physics.cpp

---
+++ PhysicsEngine/Source/Physics.cpp
@@ -0,0 +1,31 @@
+#include "Physics.h"
+
+float Physics::PhysicsClass::fForce(float mass, float acceleration)
+{
+	return mass * acceleration;
+}
+
+float Physics::PhysicsClass::Friction(float force, float frictionCoefficiant)
+{
+	return force * frictionCoefficiant;
+}
+
+float Physics::PhysicsClass::Drag(float fluidDensity, float velocity, float crossSectionSA, float dragCoefficiant)
+{
+	return (fluidDensity * (velocity * velocity) * dragCoefficiant * crossSectionSA / 2);
+}
+
+float Physics::PhysicsClass::CalculateTrajectory(float force, float mass, float drag, float angle)
+{
+	return 0;
+}
+
+float Physics::PhysicsClass::CalcVelocity(float distance, float time)
+{
+	return distance / time;
+}
+
+float Physics::PhysicsClass::CalcAcceleration(float netForce, float mass)
+{
+	return netForce / mass;
+}
