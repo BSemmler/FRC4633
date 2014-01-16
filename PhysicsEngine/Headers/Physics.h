0beab46f0a8315528c72333597b2405c0b334aa8
trag1c <trag1c@users.noreply.github.com>
2014-01-16 01:03:51 -0700

    Update Physics.h

--- PhysicsEngine/Headers/Physics.h
+++ PhysicsEngine/Headers/Physics.h
@@ -6,7 +6,7 @@ namespace Physics
 	public:
 		float fForce(float mass, float acceleration); // Input mass, acceleration
 		
-		float Friction(float force, float frictionCoefficiant);
+		float Friction(float force, float frictionCoefficiant, float forceNormal);
 		
 		float Drag(float fluidDensity, float velocity, float crossSectionSA, float dragCoefficiant);
 		
