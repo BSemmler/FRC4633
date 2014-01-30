1f138cd38184325f895bcd2cd771954d0c43d4a5
trag1c <trag1c@users.noreply.github.com>
2014-01-29 20:20:44 -0700

    Added compressor start and some debug info code
    Added code to start the compressor upon robot initialization. Also added code to send some floating point debug info to the console.

--- Robot/CPP/MyRobot.cpp
+++ Robot/CPP/MyRobot.cpp
@@ -1,29 +1,50 @@
 #include "WPILib.h"
+//#include "Drive.h"
+
+
+using namespace std;
 
 class BuiltinDefaultCode : public IterativeRobot
 {
-	// Declare Motor Controllers(Victor)8
+		// Declare Joysticks
         Joystick *rightStick, *leftStick;
+        
+        // Declare Victor Motor Controllers
         Victor *leftFrontDrive, *rightFrontDrive, *leftRearDrive, *rightRearDrive;
+        
+        // Declare Driverstation Message window
         DriverStationLCD *LCD;
         
+        // Declare Aircompressor
+        Compressor *airCompressor;
+
 public:
         BuiltinDefaultCode(void)
         {
-                // Initialize Motor Controllers(Victor) and Joysticks
+                // Initialize Joysticks and their ports
                 rightStick = new Joystick(1);
                 leftStick = new Joystick(2);
                 
+                // Initialize Victor Motor Controllers with PWM ports
                 leftFrontDrive = new Victor(4);
                 rightFrontDrive = new Victor(2);
                 leftRearDrive = new Victor(3);
                 rightRearDrive = new Victor(1);
                 
-               LCD = DriverStationLCD::GetInstance();
+                // Enable printing messages to Driver station
+                LCD = DriverStationLCD::GetInstance();
+                
+                // Create AirCompressor Object with sensor 
+                airCompressor = new Compressor(1,5);          
         }
         
         void RobotInit(void)
         {
+        	/* Once the start function is called no further programming
+        	 * is required. However if needed the Stop() function can be
+        	 * called.                                                   
+        	 * */
+        	airCompressor->Start();
         }
         
         void DisabledInit(void)
@@ -40,6 +61,7 @@ public:
         
         void DisabledPeriodic(void)
         {
+        	//airCompressor->Stop();
         }
         
         void AutonomousPeriodic(void)
@@ -60,8 +82,9 @@ public:
                 leftRearDrive->Set(-1 * leftStick->GetY());
                 rightRearDrive->Set(rightStick->GetY());
                 
+                //airCompressor->Start();
                 // Simple Button Drive Forward
-                if(rightStick->GetButton(3))
+                if(rightStick->GetRawButton(3))
                 {
                 	leftFrontDrive->Set(-1);
                 	rightFrontDrive->Set(1);
@@ -70,7 +93,7 @@ public:
                 }
                 
                 // Simple Button Drive Backwards
-                if(rightStick->GetButton(3))
+                if(rightStick->GetRawButton(2))
                 {
                 	leftFrontDrive->Set(1);
                 	rightFrontDrive->Set(-1);
@@ -80,6 +103,8 @@ public:
                 
                 // Code to print to the user messages box in the Driver Station
                 LCD->PrintfLine(DriverStationLCD::kUser_Line1, "Hello World");
+                LCD->Printf(DriverStationLCD::kUser_Line2,1,"Y Left: %f", leftStick->GetY());
+                LCD->Printf(DriverStationLCD::kUser_Line3,1,"Y Right: %f", rightStick->GetY());
                 LCD->UpdateLCD();
                 Wait(0.2); 
         }
